03733b9a85cb5bc0c37de25b4bc91ff2fc6cd67c
Shakir Ali <shakiralihamza05@gmail.com>
2022-10-05 03:33:59 +0500

    added gated content feature

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,12 +1,13 @@
-import React from "react"
-import "firebase/auth"
-import "firebase/firestore"
-import "firebase/functions"
+import React from "react";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/functions";
 
 import AuthProvider from "./src/components/Auth/AuthContext";
+import { CookiesProvider } from "react-cookie";
 
-export const wrapRootElement = ({ element }) => (
-  <AuthProvider >
+export const wrapRootElement = ({ element }) => (<AuthProvider>
+  <CookiesProvider>
     {element}
-  </AuthProvider>
-);
+  </CookiesProvider>
+</AuthProvider>);
--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -7,11 +7,16 @@ import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 import Link from "@mui/material/Link";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import { useCookies } from "react-cookie";
+import { useEffect } from "react";
+import { Alert, AlertTitle } from "@mui/material";
+import { useAuthValue } from "../components/Auth/AuthContext";
 
 export const query = graphql`
     query($slug: String!) {
         contentfulBlogPost(slug: { eq: $slug }) {
             title
+            slug
             createdAt(formatString: "Do MMMM, YYYY")
             featuredImage {
                 url
@@ -25,23 +30,76 @@ export const query = graphql`
 
 
 function BlogPost(props) {
+  const blogContent = props.data.contentfulBlogPost.body.raw;
+  const imgUrl = props.data.contentfulBlogPost.featuredImage.url;
+  const featuredImage = props.data.contentfulBlogPost.featuredImage;
+  const createdAt = props.data.contentfulBlogPost.createdAt;
+  const title = props.data.contentfulBlogPost.title;
+
+  const [cookies, setCookie] = useCookies(["postViewCount", "postsViewed"]);
+  const { currentUser } = useAuthValue();
+
+  useEffect(() => {
+    // If the user is logged in, we don't need to track their views
+    if (currentUser === null) {
+      const newPostViewCount = cookies.postViewCount ? Number(cookies.postViewCount) + 1 : 1;
+      const newPostsViewed = cookies.postsViewed ? [...cookies.postsViewed, props.data.contentfulBlogPost.slug] : [props.data.contentfulBlogPost.slug];
+
+      //if current post already visited, don't consider it
+      if (cookies.postsViewed && cookies.postsViewed.includes(props.data.contentfulBlogPost.slug)) {
+        return;
+      } else {
+        setCookie("postsViewed", newPostsViewed, { path: "/", maxAge: 3600 });
+      }
+
+      //if current post already visited, don't count it
+      if (cookies.postViewCount && cookies.postViewCount.includes(props.data.contentfulBlogPost.slug)) {
+        return;
+      } else {
+        setCookie("postViewCount", newPostViewCount, { path: "/", maxAge: 3600 });
+      }
+    }
+  }, []);
 
   return (
     <Layout>
       <Link component={GatsbyLink} to="/" sx={{ pl: 1 }}>&#60; Go Back</Link>
       <div>
-        <Typography variant={"h4"} sx={{ py: 2 }}>{props.data.contentfulBlogPost.title}</Typography>
+        <Typography variant={"h4"} sx={{ py: 2 }}>{title}</Typography>
         <Box className="meta" component={"i"}>
-          Posted on {props.data.contentfulBlogPost.createdAt}
+          Posted on {createdAt}
         </Box>
 
-        {props.data.contentfulBlogPost.featuredImage && (
-          <img src={props.data.contentfulBlogPost.featuredImage.url} alt="img" style={{ width: "100%" }} />
+        {featuredImage && (
+          <img src={imgUrl} alt="img" style={{ width: "100%" }} />
         )}
+
       </div>
-      <p>{documentToReactComponents(JSON.parse(props.data.contentfulBlogPost.body.raw))}</p>
+      <Box>
+        {
+          cookies.postViewCount > 3
+            ? currentUser === null
+              ? <GatedBlogPost postContent={blogContent} />
+              : <FullBlogPost postContent={blogContent} />
+            : <FullBlogPost postContent={blogContent} />
+        }
+      </Box>
     </Layout>
   );
 }
+
+const FullBlogPost = ({ postContent }) => <>{documentToReactComponents(JSON.parse(postContent))}</>;
+
+const GatedBlogPost = ({ postContent }) => (<>
+  {
+    // @ts-ignore
+    documentToReactComponents(JSON.parse(postContent)).slice(0, 3)
+  }
+  <blockquote><b>...</b></blockquote>
+  <Alert severity="warning">
+    <AlertTitle>Sorry, you've reached your limit</AlertTitle>
+    Please Sign Up to view full blog post, or try again in an hour
+  </Alert>
+</>);
 
 export default BlogPost;
